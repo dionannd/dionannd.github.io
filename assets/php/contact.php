8aac0d30b3f49572753d229a35c1f79942d4e8e7
Dian Ananda <dionananda77@gmail.com>
2019-09-08 22:27:53 +0700

    fix portfolio video

--- assets/php/contact.php
+++ assets/php/contact.php
@@ -1,30 +1,31 @@
 <?php
 
-    // Here we get all the information from the fields sent over by the form.
-    $name = $_REQUEST['user-name'];
-    $email = $_REQUEST['user-email'];
-    $message = ( $_REQUEST['user-message'] ) ? $_REQUEST['user-message'] : '';
-    $status = $_REQUEST['user-status'];
+// Here we get all the information from the fields sent over by the form.
+$name    = $_REQUEST['user-name'];
+$email   = $_REQUEST['user-email'];
+$message = ($_REQUEST['user-message']) ? $_REQUEST['user-message'] : '';
+$status  = $_REQUEST['user-status'];
 
-    $to = 'youremail@domain.com';
-    $message = 'Name: '.$name.'<br /> Email: '.$email.'<br />Message: '.$message;
+$to      = 'dionananda77@gmail.com';
+$message = 'Name: ' . $name . '<br /> Email: ' . $email . '<br />Message: ' . $message;
 
-    $subject = 'Subject';
+$subject = 'Subject';
 
-    $headers = "From: You company <".$to.">". "\r\n" .
-                  "Return-Path: You company <info@yousite.com>\r\n".
-                  "Reply-To: You company <".$to.">" . "\r\n" .
-                  "MIME-Version: 1.0\r\n".
-                  "Content-type: text/html; charset=iso-8859-1\r\n".
-                  "X-Priority: 3\r\n" .
-                  'X-Mailer: PHP/' . phpversion();
+$headers = "From: Dionannd <" . $to . ">" . "\r\n" .
+"Return-Path: You company <dionananda77@gmail.com>\r\n" .
+"Reply-To: Dionannd <" . $to . ">" . "\r\n" .
+"MIME-Version: 1.0\r\n" .
+"Content-type: text/html; charset=iso-8859-1\r\n" .
+"X-Priority: 3\r\n" .
+'X-Mailer: PHP/' . phpversion();
 
-    if (filter_var($email, FILTER_VALIDATE_EMAIL) && $status == "yes" ) { // shis line checks that we have a valid email address
-        mail($to, $subject, $message, $headers); // this method sends the mail.
-        echo "success"; // success message
-        exit;
-    }else{
-        echo "error"; //error
-    }
+if (filter_var($email, FILTER_VALIDATE_EMAIL) && $status == "yes") {
+    // shis line checks that we have a valid email address
+    mail($to, $subject, $message, $headers); // this method sends the mail.
+    echo "success"; // success message
+    exit;
+} else {
+    echo "error"; //error
+}
 
 ?>
