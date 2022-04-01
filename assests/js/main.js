a1ddec6c7eb2d69816d1db8c87938cdd515eec50
dionannd <dionananda77@gmail.com>
2022-04-01 13:33:38 +0700

    feat: add other info

--- assests/js/main.js
+++ assests/js/main.js
@@ -43,3 +43,74 @@ function scrollActive() {
   });
 }
 window.addEventListener("scroll", scrollActive);
+
+/* Show scroll top */
+function scrollTop() {
+  const scrollTop = document.getElementById("scroll-top");
+  if (this.scrollY >= 200) scrollTop.classList.add("show-scroll");
+  else scrollTop.classList.remove("show-scroll");
+}
+window.addEventListener("scroll", scrollTop);
+
+// Dark Light Theme
+const themeButton = document.getElementById("theme-button");
+const darkTheme = "dark-theme";
+const iconTheme = "bx-sun";
+
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("selected-icon");
+
+const getCurrentTheme = () =>
+  document.body.classList.contains(darkTheme) ? "dark" : "light";
+const getCurrentIcon = () =>
+  themeButton.classList.contains(iconTheme) ? "bx-moon" : "bx-sun";
+
+if (selectedTheme) {
+  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+    darkTheme
+  );
+  themeButton.classList[selectedIcon === "bx-moon" ? "add" : "remove"](
+    iconTheme
+  );
+}
+
+themeButton.addEventListener("click", () => {
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
+  localStorage.setItem("selected-theme", getCurrentTheme());
+  localStorage.setItem("selected-icon", getCurrentIcon());
+});
+
+// Reduce size & print A4 size
+function scaleCv() {
+  document.body.classList.add("scale-cv");
+}
+
+// Remove scale
+function removeScale() {
+  document.body.classList.remove("scale-cv");
+}
+
+// Generate PDF
+let areaCv = document.getElementById("area-cv");
+let resumeButton = document.getElementById("resume-button");
+
+// html2pdf Options
+let opt = {
+  margin: 0,
+  filename: "myResume.pdf",
+  image: { type: "jpeg", quality: 0.98 },
+  html2canvas: { scale: 4 },
+  jsPDF: { format: "a4", orientation: "portrait" },
+};
+
+// areaCv & html2pdf options
+function generateResume() {
+  html2pdf(areaCv, opt);
+}
+
+// button Generate clicked
+resumeButton.addEventListener("click", () => {
+  scaleCv();
+  generateResume();
+});
